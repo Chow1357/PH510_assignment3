6ba6ee1bdbdd2b67b72bef9808b19add8f971cd4
Callum Howie <kwb21127@archie-e.hpc.strath.ac.uk>
2026-03-16 18:39:19 +0000

    testing for a million walkers

--- Task2_optimised.py
+++ Task2_optimised.py
@@ -148,7 +148,7 @@ if __name__ == "__main__":
     N = 50 
 
     # number of walkers across all MPI ranks
-    nwalkers = 100000
+    nwalkers = 1000000
 
     # test point near the centre 
     start_i = 25 
