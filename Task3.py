2e5d9ca09b8e4c4145fa04232fbdc9bac3c3e90d
Callum Howie <kwb21127@archie-e.hpc.strath.ac.uk>
2026-03-16 14:47:02 +0000

    added the boundary conditions and single walker test to task 3 as well as the necessary numpy and plotting functions

--- Task3.py
+++ Task3.py
@@ -1,3 +1,45 @@
+#!/opt/software/anaconda/python-3.10.9/bin/python
+import numpy as np
+import matplotlib
+matplotlib.use("Agg")
+import matplotlib.pyplot as plt
+from mpi4py import MPI
+
+comm = MPI.COMM_WORLD
+rank = comm.Get_rank()
+size = comm.Get_size()
+
+#
+def is_boundary(i, j, N):
+    """
+    function determines when the walker has
+    reached the absorbing boundary
+    """
+    return i == 0 or i == N+1 or j == 0 or j == N+1
+# One random walk test
+def single_walk(start_i, start_j, N, rng):
+    """
+    performs a random walk from a start point on the grid (start_i, start_j)
+    and creates an array and records how many times this walker visits each point.
+    """
+    visits = np.zeros((N+2, N+2), dtype=int)
+
+    i, j = start_i, start_j
+
+    while not is_boundary(i, j, N):
+        visits[i, j] += 1
+        # this matches 
+        step = rng.integers(4) # random number generator with equal probability (directions) 
+        if step == 0:
+            i += 1
+        elif step == 1:
+            i -= 1
+        elif step == 2:
+            j += 1
+        else:
+            j -= 1
+    return visits
+
 #--------------------------
 # TASK 3
 #--------------------------
