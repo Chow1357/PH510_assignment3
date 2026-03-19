90876cfad4f8655bfbd7655412c73661b85578a5
Callum Howie <kwb21127@archie-e.hpc.strath.ac.uk>
2026-03-19 18:41:58 +0000

    Add Task 3 Green's function and plots for selected points

--- Task3.py
+++ Task3.py
@@ -16,31 +16,32 @@ size = comm.Get_size()
 # to a corresponding position on the grid
 def physical_to_grid(x_cm, y_cm, L_cm, N):
     """
+    Convert physical coordinates in cm to interior grid indices.
     """
 
     h = L_cm / (N+1)
     
-    i = int(round(x_cm / h))
-    j = int(round(y_cm / h))
+    i = int(round(y_cm / h))
+    j = int(round(x_cm / h))
 
     # clamping values so they stay within the grid
     i = max(1, min(N, i))
     j = max(1, min(N, j))
+
     return i, j
 
 if __name__ == "__main__":
     N = 50 
     nwalkers = 200000
-    L = 100 
+    L = 100  # cm
 
     # defining the different points to test on the grid in length scale
-    points = [(50, 50), (2, 2), (2, 50)] # in cm 
+    points = {"centre": (50, 50),"corner": (2, 2),"face": (2, 50)}  
 
-    for x_cm, y_cm in points: 
- 
+    for name, (x_cm, y_cm) in points.items(): 
         start_i, start_j = physical_to_grid(x_cm, y_cm, L, N)
  
-        G, G_std, G_stderr, mean_visits, std_visits = estimate_greens_function(start_i, start_j, N, nwalkers, factor=0.25, seed=1234, chunk_size=2500)
+        G, G_std, G_stderr, mean_visits, std_visits, boundary_prob = estimate_greens_function(start_i, start_j, N, nwalkers, factor=0.25, seed=1234, chunk_size=2500)
 
         if rank == 0:
             print()
@@ -50,7 +51,7 @@ if __name__ == "__main__":
             print("Green's function:", G[start_i, start_j])
             print("Standard deviation:", G_std[start_i, start_j])
             print("Standard error:", G_stderr[start_i, start_j])
-
+            print(f"Sum of boundary probabilities: {np.sum(boundary_prob):.6f}")
             plt.figure(figsize=(6, 5))
             plt.imshow(G[1:N+1, 1:N+1], origin="lower", extent=[0, L, 0, L])
             plt.colorbar(label="Green's function")
