293fc38f4db6503393c79f6cf7d337cb209bd0c2
Callum Howie <kwb21127@archie-e.hpc.strath.ac.uk>
2026-03-19 20:07:42 +0000

    converting physical points to grid points locally in task 4

--- Task4_firstpart.py
+++ Task4_firstpart.py
@@ -7,4 +7,11 @@ comm = MPI.COMM_WORLD
 rank = comm.Get_rank()
 
 #converting physcal points to grid points
+def physical_to_grid(x_cm, y_cm, L_cm, N):
+    h = L_cm / (N + 1)
+    i = int(round(y_cm / h))
+    j = int(round(x_cm / h))
+    i = max(1, min(N, i))
+    j = max(1, min(N, j))
+    return i, j
 
