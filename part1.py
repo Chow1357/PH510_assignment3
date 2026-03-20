a535cef635d740fe3f4c55976b48ba888924ee22
Callum Howie <kwb21127@archie-s.hpc.strath.ac.uk>
2026-03-20 17:35:25 +0000

    adding license to each code

--- part1.py
+++ part1.py
@@ -1,5 +1,30 @@
 #!/opt/software/anaconda/python-3.10.9/bin/python
-"""Solve the 2D Poisson equation on a square grid using SOR """
+"""
+thsi code is run with python3.10.9
+Solve the 2D Poisson equation on a square grid using SOR 
+
+MIT License
+
+Copyright (c) 2026 Chow1357
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
 import matplotlib
 matplotlib.use("Agg")
 import matplotlib.pyplot as plt
